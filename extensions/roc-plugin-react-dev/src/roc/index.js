ba9bf2af24950babcacf9ca2a7c931597690adcb
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-10-05 19:17:24 +0200

    Updated to use the new Babel management system

--- extensions/roc-plugin-react-dev/src/roc/index.js
+++ extensions/roc-plugin-react-dev/src/roc/index.js
@@ -1,7 +1,4 @@
-import webpack from '../webpack';
-
 export default {
-    plugins: [require.resolve('roc-plugin-react')],
     config: {
         settings: {
             dev: {
@@ -12,8 +9,30 @@ export default {
         },
     },
     actions: [{
-        hook: 'build-webpack',
-        description: 'Adds the needed Webpack configuration to be able to build and develop React code.',
-        action: webpack,
+        hook: 'babel-config',
+        description: 'Adds the needed Babel configuration to be able to build and develop React code.',
+        action: ({ context: { config } }) => (target) => (babelConfig) => {
+            babelConfig.presets.push(require.resolve('babel-preset-react'));
+
+            if (target === 'web' && config.settings.build.mode === 'dev') {
+                babelConfig.plugins.push([
+                    require.resolve('babel-plugin-react-transform'), {
+                        transforms: [
+                            {
+                                transform: require.resolve('react-transform-hmr'),
+                                imports: ['react'],
+                                locals: ['module'],
+                            },
+                            {
+                                transform: require.resolve('react-transform-catch-errors'),
+                                imports: ['react', require.resolve('redbox-react')],
+                            },
+                        ],
+                    },
+                ]);
+            }
+
+            return babelConfig;
+        },
     }],
 };
