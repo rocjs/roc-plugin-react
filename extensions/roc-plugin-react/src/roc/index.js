510f03924cea1d552baf864df7c854a429dc992e
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2017-04-20 20:59:56 +0200

    Export the standalone prop-types module and bump dependencies

--- extensions/roc-plugin-react/src/roc/index.js
+++ extensions/roc-plugin-react/src/roc/index.js
@@ -5,6 +5,7 @@ const packageJSON = require('../../package.json');
 export default {
     dependencies: {
         exports: generateDependencies(packageJSON, [
+            'prop-types',
             'react',
             'react-dom',
         ]),
